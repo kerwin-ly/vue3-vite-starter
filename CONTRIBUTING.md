255e9246ebbb7966bcb8cc9b6a4895457625375e
kerwin <879688355@qq.com>
2022-06-17 18:02:17 +0800

    docs: update README.md

---
+++ CONTRIBUTING.md
@@ -0,0 +1,5 @@
+# Contributing
+
+## IDE
+
+Please use [VSCode](https://code.visualstudio.com/) to programming and install `Prettier`, `ESLint`, `Stylelint` and `Volar` to format your code.
--- commitlint.config.js
+++ commitlint.config.js
@@ -6,7 +6,7 @@ module.exports = {
     'header-max-length': [2, 'always', 72],
     'scope-case': [2, 'always', 'lower-case'],
     'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
-    'subject-empty': [2, 'never'],
+    // 'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
